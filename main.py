06196edd6968775a11c09b254d1191a66b9dc158
Aljiquita <98359502+Aljiquita@users.noreply.github.com>
2023-05-12 14:52:16 -0500

    25-05-2023

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ app = FastAPI()
 
 # en render.com mlocale -ae crea problemas por _setlocale
 #  y para corregir el problema se ejecuta el cambio de localidad 
-locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
+
 
 df = pd.read_csv('./DataSet/movies_dataset_normalizado.csv' )
 
 
 
 # A-) def peliculas_mes(mes): 
-df["release_date"] = pd.to_datetime(df["release_date"])
-df["release_month_name"] = df['release_date'].dt.month_name(locale='es')
+
 
 
 @app.get("/peliculas_mes/{mes}")
 def peliculas_mes(mes: str):
     '''Se ingresa el mes y la función retorna la cantidad de películas que se estrenaron ese mes históricamente'''
+    df["release_date"] = pd.to_datetime(df["release_date"])
+    df["release_month_name"] = df['release_date'].dt.month_name()#locale='es'
     respuesta = df["release_month_name"][df["release_month_name"] == mes].count()
     if respuesta > 0:
         return {'mes':mes, 'cantidad': f"{respuesta}"  }
@@ -36,11 +37,13 @@ def peliculas_mes(mes: str):
 
 
 # B-) def peliculas_dia(dia): 
-df["release_day_name"] = df['release_date'].dt.day_name(locale='es')
+
 
 @app.get("/peliculas_dia/{dia}")
 def peliculas_dia(dia: str):
     '''Se ingresa el dia y la función retorna la cantidad de peliculas que se estrenaron ese dia históricamente'''
+    df["release_date"] = pd.to_datetime(df["release_date"])
+    df["release_day_name"] = df['release_date'].dt.day_name()#locale='es'
     respuesta = df["release_day_name"][df["release_day_name"]== dia].count()
     if respuesta > 0:
         return {'dia':dia, 'cantidad':f"{respuesta}"}
